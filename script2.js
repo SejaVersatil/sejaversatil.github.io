4209c1e0210fbf485b0479f91572e59c28784387
SejaVersatil <gptricksyt@gmail.com>
2025-11-14 10:01:35 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -68,7 +68,7 @@ const heroSlides = [
         cta: 'EXPLORAR AGORA'
     },
     {
-        image: 'https://i.imgur.com/VBoPWxI.jpeg',
+        image: 'https://i.imgur.com/WeSHB5P.jpeg',
         title: 'LANÇAMENTO',
         subtitle: 'Tecnologia para máxima performance',
         cta: 'VER COLEÇÃO'
@@ -2690,6 +2690,7 @@ document.addEventListener('visibilitychange', function() {
 });
 
 // ==================== FIM DO ARQUIVO ====================
+
 
 
 
