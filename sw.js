73a40cf9e3ca15fa9a6e126f754835c1ccb8681f
SejaVersatil <gptricksyt@gmail.com>
2025-11-15 16:34:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seja-versatil-v1.0.0';
+const CACHE_NAME = 'seja-versatil-v1.0.1';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -60,3 +60,4 @@ self.addEventListener('fetch', (event) => {
             })
     );
 });
+
